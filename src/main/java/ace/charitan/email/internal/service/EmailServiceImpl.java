cbc76e32df172d481f439993968f9fc24d866a6d
Nguyen Do <khoanguyen.do@outlook.com>
2025-01-08 21:17:09 +0700

    feat: implement logger

--- src/main/java/ace/charitan/email/internal/service/EmailServiceImpl.java
+++ src/main/java/ace/charitan/email/internal/service/EmailServiceImpl.java
@@ -2,6 +2,8 @@ package ace.charitan.email.internal.service;
 
 import ace.charitan.email.external.service.ExternalEmailService;
 import jakarta.mail.internet.MimeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -9,24 +11,26 @@ import org.springframework.stereotype.Service;
 
 @Service
 class EmailServiceImpl implements ExternalEmailService, InternalEmailService {
-    @Autowired
-    private JavaMailSender emailSender;
+    final private JavaMailSender mailSender;
+    final private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private EmailServiceImpl(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
     @Override
     public void sendEmail(String to, String subject, String text) {
-        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessage message = mailSender.createMimeMessage();
         try {
             MimeMessageHelper helper = new MimeMessageHelper(message, false);
+            helper.setFrom("noreply@charitan.com");
             helper.setTo(to);
             helper.setSubject(subject);
             helper.setText(text, true);
-            emailSender.send(message);
+            mailSender.send(message);
+            logger.info("Sent an email to [{}]", to);
         } catch (Exception e) {
-            System.out.print(e.getMessage());
+            logger.error("An error occurred while sending an email to [{}], details: {}", to, e.getMessage());
         }
     }
-
-
-
-
 }
