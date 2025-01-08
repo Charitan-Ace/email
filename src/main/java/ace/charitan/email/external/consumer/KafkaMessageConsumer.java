f0789be56101d44e9d9d82eccc536cc745af227e
Nguyen Do <khoanguyen.do@outlook.com>
2025-01-08 21:17:41 +0700

    feat: implements auth and project management listener

--- src/main/java/ace/charitan/email/external/consumer/KafkaMessageConsumer.java
+++ src/main/java/ace/charitan/email/external/consumer/KafkaMessageConsumer.java
@@ -1,20 +1,50 @@
 package ace.charitan.email.external.consumer;
 
-
 import ace.charitan.common.dto.donation.SendDonationNotificationDto;
+import ace.charitan.common.dto.email.auth.EmailAuthCreationDto;
+import ace.charitan.common.dto.email.auth.EmailAuthVerificationDto;
+import ace.charitan.common.dto.email.project.EmailProjectApproveDto;
+import ace.charitan.common.dto.email.project.EmailProjectHaltCharityDto;
+import ace.charitan.common.dto.email.project.EmailProjectHaltDonorDto;
 import ace.charitan.email.external.service.ExternalEmailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 class KafkaMessageConsumer {
-    @Autowired
-    private ExternalEmailService service;
+    final private ExternalEmailService service;
 
-    @KafkaListener(topics = "donation-email", groupId = "email")
+    private KafkaMessageConsumer(ExternalEmailService service) {
+        this.service = service;
+    }
+
+    @KafkaListener(topics = "donation-email")
     public void listen(SendDonationNotificationDto dto) {
         service.sendEmail("thienphucdoantran@gmail.com", "lol", "lol");
     }
 
+    @KafkaListener(topics = "email.auth.creation")
+    public void listen(EmailAuthCreationDto dto) {
+        service.sendEmail(dto.recipient(), dto.subject(), dto.body());
+    }
+
+    @KafkaListener(topics = "email.auth.verification")
+    public void listen(EmailAuthVerificationDto dto) {
+        service.sendEmail(dto.recipient(), dto.subject(), dto.body());
+    }
+
+    @KafkaListener(topics = "email.project.approval")
+    public void listen(EmailProjectApproveDto dto) {
+        service.sendEmail(dto.recipient(), dto.subject(), dto.body());
+    }
+
+    @KafkaListener(topics = "email.project.halt.charity")
+    public void listen(EmailProjectHaltCharityDto dto) {
+        service.sendEmail(dto.recipient(), dto.subject(), dto.body());
+    }
+
+    @KafkaListener(topics = "email.project.halt.donor")
+    public void listen(EmailProjectHaltDonorDto dto) {
+        service.sendEmail(dto.recipient(), dto.subject(), dto.body());
+    }
 }
